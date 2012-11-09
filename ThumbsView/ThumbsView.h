75c72799f833823365036520185aa6d755a4b1ec
Jason Rush <jason@fizzawizza.com>
2012-11-09 16:44:33 -0600

    Initial Commit

---
+++ ThumbsView/ThumbsView.h
@@ -0,0 +1,13 @@
+//
+//  ThumbsView.h
+//  ThumbsView
+//
+//  Created by Jason Rush on 11/9/12.
+//  Copyright (c) 2012 Fizzawizza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThumbsView : NSObject
+
+@end
---
+++ ThumbsView/ThumbsView.m
@@ -0,0 +1,13 @@
+//
+//  ThumbsView.m
+//  ThumbsView
+//
+//  Created by Jason Rush on 11/9/12.
+//  Copyright (c) 2012 Fizzawizza. All rights reserved.
+//
+
+#import "ThumbsView.h"
+
+@implementation ThumbsView
+
+@end
